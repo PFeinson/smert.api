0be9e80179e5d9b5c34502734aa8718be6eb66df
ThermalVB <wyatt.horejsi@gmail.com>
2021-11-11 09:30:45 -0800

    feat: Communication_Result

---
+++ Models/Communication_Result.cs
@@ -0,0 +1,8 @@
+using System.DateTime
+
+public class Communication_Result{
+    private int communication_result_id {get; set;}
+    private string communication_result_name {get; set;}
+    private DateTime create_timestamp {get; set;}
+    private int is_active {get; set;}
+}
