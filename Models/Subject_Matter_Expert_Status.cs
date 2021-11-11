a98f53a8f245a5b3b10d0fabb212edac6fdf80b0
ThermalVB <wyatt.horejsi@gmail.com>
2021-11-11 13:17:42 -0800

    fixup! feat: Subject_Matter_Expert_Status model

---
+++ Models/Subject_Matter_Expert_Status.cs
@@ -0,0 +1,8 @@
+using System;
+
+public class Subject_Matter_Expert_Status{
+    private int subject_matter_expert_status_id {get; set;}
+    private string subject_matter_expert_status_name {get; set;}
+    private System.DateTime create_timestamp {get; set;}
+    private int is_active {get; set;}
+}
