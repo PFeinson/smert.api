fe99a3865aeb4f1d6880c7282e4c0e20242dea49
ThermalVB <wyatt.horejsi@gmail.com>
2021-11-11 11:26:16 -0800

    feat: Role model

---
+++ Models/Role.cs
@@ -0,0 +1,8 @@
+using System.DateTime;
+
+public class Role{
+    private int role_id {get; set;}
+    private string role_name {get; set;}
+    private DateTime create_timestamp {get; set:}
+    private int is_active {get; set;}
+}
