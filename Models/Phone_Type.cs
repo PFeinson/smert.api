e2eafb99211f44c14685cc015b7c211b805373a9
ThermalVB <wyatt.horejsi@gmail.com>
2021-11-11 11:24:35 -0800

    fixup! feat: Phone_Type model

---
+++ Models/Phone_Type.cs
@@ -0,0 +1,7 @@
+using System.DateTime;
+
+public class Phone_Type{
+    private int phone_type_id {get; set;}
+    private string phone_type_name {get; set;}
+    private DateTime create_timestamp {get; set;}
+}
