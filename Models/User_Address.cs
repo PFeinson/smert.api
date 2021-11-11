dc5eabc67308a8caf6d8e6905570e2e38a625366
ThermalVB <wyatt.horejsi@gmail.com>
2021-11-11 13:31:09 -0800

    fixup! feat: User_Address model

---
+++ Models/User_Address.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class User_Address{
+    private int user_address_id {get; set;}
+    private int user_id {get; set;}
+    private string full_address {get; set;}
+    private string city {get; set;}
+    private string state {get; set;}
+    private string postal_code {get; set;}
+    private string zip4 {get; set;}
+    private double latitude {get; set;}
+    private double longitude {get; set;}
+    private int is_primary_address {get; set;}
+    private DateTime create_timestamp {get; set;}
+    private int create_user_id {get; set;}
+    private DateTime suppress_timestamp {get; set;}
+    private int suppress_user_id {get; set;}
+}
