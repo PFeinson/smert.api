326c61f1317b55ce7a08be7dbb4b191dfc1f2a89
ThermalVB <wyatt.horejsi@gmail.com>
2021-11-11 09:09:43 -0800

    feat: Applicant_Status model

---
+++ Models/Address_Type.cs
@@ -0,0 +1,7 @@
+using System.DateTime;
+public class Address_Type{
+    private int address_type {get; set;}
+    private string communication_result_name {get; set;}
+    private DateTime create_timestamp {get; set;}
+    private int is_active {get; set;}
+}
