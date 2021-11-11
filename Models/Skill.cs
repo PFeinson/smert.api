6fca4d4cafda203b30fc684e9316677f038e490e
ThermalVB <wyatt.horejsi@gmail.com>
2021-11-11 13:14:58 -0800

    feat: Skill model

---
+++ Models/Skill.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Skill{
+    private int skill_id{get; set;}
+    private string skill_name {get; set;}
+    private DateTime create_timestamp {get; set;}
+    private int create_user_id {get; set;}
+    private int is_active {get; set;}
+}
