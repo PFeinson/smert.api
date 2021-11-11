ef6799474232901bf5ac0478cee6a966ee8c264f
ThermalVB <wyatt.horejsi@gmail.com>
2021-11-11 09:19:15 -0800

    fixup! feat: Candidate_Status model

---
+++ Models/Candidate_Status.cs
@@ -0,0 +1,7 @@
+using System.DateTime;
+
+public class Candidate_Status{
+    private int candidate_status_id {get; set;}
+    private string candidate_status_name {get; set;}
+    private DateTime create_timestamp {get; set;}
+}
